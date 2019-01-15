fabca25f0dbb0fb5b59d455777b77c66a3c6283d
Unknown <riderjensen@gmail.com>
2019-01-15 14:01:32 -0700

    Dummy post controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,3 +6,18 @@ exports.getPosts = (req, res, next) => {
 		}]
 	});
 };
+
+exports.createPost = (req, res, next) => {
+	const title = req.body.title;
+	const content = req.body.content;
+	// create post in db
+	res.status(201).json({
+		message: 'Post created',
+		post: {
+			id: new Date().toISOString(),
+			title: title,
+			content,
+			content
+		}
+	})
+};
