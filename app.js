cfce40952a88c6288fd3c1fbb4e9b496f4046170
Unknown <riderjensen@gmail.com>
2019-01-16 12:51:24 -0700

    Multer storage and error handling

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const path = require('path');
+const uuidv4 = require('uuid/v4');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'images');
+	},
+	filename: function (req, file, cb) {
+		cb(null, uuidv4() + '-' + file.originalname)
+	}
+});
+
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 app.use(bodyParser.json());
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
@@ -19,6 +45,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.stausCode || 500;
+	const message = error.message;
+	res.status(status).json({ message: message });
+});
 
 mongoose.connect('mongodb+srv://rider:12345678Ah@nodecourse-zfafv.mongodb.net/messages')
 	.then(result => {
