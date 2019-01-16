606b77e953b4414acb1a9f2a6aa811aef1f12d61
Unknown <riderjensen@gmail.com>
2019-01-16 09:46:51 -0700

    Adding mongoose link and static path

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,4 +20,8 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://rider:12345678Ah@nodecourse-zfafv.mongodb.net/messages')
+	.then(result => {
+		app.listen(8080);
+	})
+	.catch(err => console.log(err));
