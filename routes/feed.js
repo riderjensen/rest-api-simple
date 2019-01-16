e3eff9a13f3c4710e48ab4d918c8bc82dc77a20d
Unknown <riderjensen@gmail.com>
2019-01-16 13:23:51 -0700

    Add semicolon

--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator/check')
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 
