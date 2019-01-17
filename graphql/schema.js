b6c6eedff9afefb34b0de3bc55dfe3c7af1cbcaa
Unknown <riderjensen@gmail.com>
2019-01-17 14:54:48 -0700

    More inputs into schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -22,18 +22,36 @@ module.exports = buildSchema(`
 		posts: [Post!]!
 	}
 
+	type PostData {
+		posts: [Post!]!
+		totalPosts: Int!
+	}
+
 	input UserInputData {
 		email: String!
 		name: String!
 		password: String!
 	}
 
+	input PostInputData {
+		title: String!
+		content: String!
+		imageUrl: String!
+	}
+
 	type RootMutation {
 		createUser(userInput: UserInputData): User!
+		createPost(postInput: PostInputData): Post!
+	}
+
+	type AuthData {
+		token: String!
+		userId: String!
 	}
 
 	type RootQuery {
-		hello: String
+		login(email: String!, password: String!): AuthData!
+		posts(page: Int): PostData!
 	}
 
 	schema {
